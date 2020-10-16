919770784715729baa81ddf931b291fd3f08666a
Diego Didona <ddi@zurich.ibm.com>
2020-10-16 11:15:00 +0200

    Asserting alignment in IOU only for uncached

--- fdbrpc/AsyncFileIOUring.actor.h
+++ fdbrpc/AsyncFileIOUring.actor.h
@@ -648,8 +648,8 @@ private:
 	}
 
 	void enqueue( IOBlock* io, const char* op, AsyncFileIOUring* owner ) {
-		printf("URING enquein data size %lu for op %s\n",int64_t(io->buf),op);
-		ASSERT( int64_t(io->buf) % 4096 == 0);
+		printf("URING enquein data size %lu for op %s. Uncached is %d\n",int64_t(io->buf),op,bool(flags & IAsyncFile::OPEN_UNCACHED));
+		ASSERT( bool(flags & IAsyncFile::OPEN_UNCACHED) && int64_t(io->buf) % 4096 == 0);
 	        ASSERT(io->offset % 4096 == 0);
 		ASSERT( io->nbytes % 4096 == 0 );
 
