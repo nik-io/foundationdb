2c62f331f6753f720704a8e2c76e077a7dda06ef
Diego Didona <ddi@zurich.ibm.com>
2021-01-09 11:36:56 +0100

    Separate config file

--- iou-acceptance/asyncfile.sh
+++ iou-acceptance/asyncfile.sh
@@ -7,22 +7,7 @@
 AsyncFileTest
 END_COMM
 
-FDBCLI="/mnt/ddi/uringdb/bld/bin/fdbcli"
-FDBSERVER="/mnt/ddi/uringdb/bld/bin/fdbserver"
-LIB="/mnt/ddi/uringdb/liburing/src"
-#use .stub for the stub and .txt for the test
-TEST="/mnt/ddi/uringdb/tests/IOU"
-CLS="/home/ddi/fdb-official/fdb.zac13"
-#device on which  the data and log path are mounted (used for io stat collection)
-MOUNT_POINT="/mnt/nvme/nvme0"
-DEV="nvme0n1"
-DATALOGPATH=${MOUNT_POINT}"/ioutest"
-FILEPATH=${MOUNT_POINT}"/testfiles"
-PAGE_CACHE="10"  #MiB
-RESULTS=`date +%Y-%m-%d_%H-%M-%S`
-hn=$(hostname)
-RESULTS="${RESULTS}-${hn}-ASYNC"
-RESULTS="debug"
+source config.sh || exit 1
 mkdir -p ${RESULTS} || exit 1
 port=
 
