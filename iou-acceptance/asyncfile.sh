2c3f30836d055c62d3695aa39c1672ec729d36a1
Diego Didona <ddi@zurich.ibm.com>
2021-01-11 17:32:21 +0100

    script

--- iou-acceptance/asyncfile.sh
+++ iou-acceptance/asyncfile.sh
@@ -27,7 +27,7 @@ testport=
 
 uring=""
 
-TRIM=0
+TRIM=1
 #If TRIM is enabled, the file has to be retrieved from somewhere to avoid creating it every time
 #Be sure that the name of the file matches the field in the test file
 PRE_TEST_FILE="/mnt/ddi/file.dat"
@@ -75,10 +75,13 @@ spawn(){
 
 	if [[ $TRIM == 1 ]];then
 	#if [[ true ]]; then
-		echo "Copying $fn to $PRE_TEST_FILE"
+		echo "Copying  $PRE_TEST_FILE to $fn"
 		cp $PRE_TEST_FILE $fn
 		echo "Finished copying"
 		sync $fn
+		echo "Sync'd"
+		sudo sh -c "echo '3' > /proc/sys/vm/drop_caches"
+		echo "Cache dropped"
 		while [ $(echo "$(iostat 1 1 -y| grep $DEV | awk {'print $4'}) >= 4096" | bc -l) == "1" ];do echo "not quiescent" ;sleep 5; done
 	fi
 
@@ -126,7 +129,7 @@ setup_test(){
 
 		echo "Trimming /dev/$DEV"
 		sudo /sbin/blkdiscard /dev/$DEV
-		yes | sudo mkfs.ext4 /dev/$DEV -E nodiscard
+		yes | sudo mkfs.ext4 /dev/$DEV -E lazy_itable_init=0,lazy_journal_init=0,nodiscard
 		if [[ $? -ne 0 ]]; then
 			echo "ext4 failed"
 			exit 1
@@ -228,10 +231,10 @@ buff="unbuffered" #buffered unbuffered
 cached="uncached"   #cached uncached
 
 for b in "unbuffered"; do
-	for c in "uncached";do
+	for c in "uncached" "cached";do
 		for run in 1 2 3 4 5; do
-			for parallel_reads in 64; do
-				for write_perc in 0 0.;do
+			for parallel_reads in 1 32 64; do
+				for write_perc in 0 0.5 1;do
 					for io in  "io_uring" "kaio"; do
 						run_one ${io} ${sec} ${parallel_reads} ${b} ${c} ${write_perc} ${run}
 					done #uring
